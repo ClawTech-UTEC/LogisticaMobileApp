4c119523cd41b92aedfd194afaf5fb07f5dac206
Guillermo Rodriguez <48388462+guille-developer@users.noreply.github.com>
2022-06-21 22:25:53 -0300

    se agrega clase reporte data

---
+++ lib/models/reporte_data.dart
@@ -0,0 +1,12 @@
+class ReporteData {
+  int year;
+  int mes;
+  String idProducto;
+  int cantidad;
+
+  ReporteData(
+      {required this.year,
+      required this.mes,
+      required this.idProducto,
+      required this.cantidad});
+}
